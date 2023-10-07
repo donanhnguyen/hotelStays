5f4f8a253d8a696cbbf7d9049571ef35b2286386
Don Anh Nguyen <thedonwind69@gmail.com>
2023-10-07 12:43:40 -0500

    API set up

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,10 +1,17 @@
 import User from "../models/User.js";
+import bcrypt from 'bcryptjs'
 
 export const register = async (req, res, next) => {
     try {
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
         const newUser = new User({
             username: req.body.username,
-            password: req.body.password
+            email: req.body.email,
+            password: hash,
+          
         });
 
         await newUser.save();
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,7 @@
 import express from "express";
-import User from '../models/User.js';
-import { createUser, logInUser } from "../controllers/user.js";
+import {logInUser } from "../controllers/user.js";
 
 const router = express.Router();
-
-// create user/sign up
-router.post('/', createUser)
 
 // get user /  log in
 router.get('/:username', logInUser)
